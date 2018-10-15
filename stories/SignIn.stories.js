b0d5faf1be5c75d99bb7c596401d360e98327ec7
Artem Kuskis <artem.kuskis@gmail.com>
2018-10-15 21:29:01 +0300

    Extract "onChange" in "SignIn" component (#7)
    * Proper name of stories
    
    * Extract onChange into 'form' element

--- stories/SignIn.stories.js
+++ stories/SignIn.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 
-import './MenuItem.css';
+import './SignIn.css';
 import SignIn from '../src/components/SignIn';
 
 storiesOf('SignIn', module)
